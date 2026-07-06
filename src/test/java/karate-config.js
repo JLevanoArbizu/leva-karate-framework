df9625ca8ea3b00d51dbd28f53447a14807063cb
JLevanoArbizu <jlevanoarbizu@gmail.com>
2026-07-06 09:30:51 -0500

    feat: Actualizacion yml

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,5 +1,4 @@
 function fn() {
-    // 1. Inicializamos un objeto vacío que contendrá nuestras variables globales
     var config = {
         authClientId: '',
         authClientSecret: '',
@@ -7,21 +6,37 @@ function fn() {
         authPassword: ''
     };
 
-    try {
-        // 2. Leemos el archivo JSON usando la función nativa de Karate
-        var secretosLocales = karate.read('file:config/secrets.json');
-        karate.log('✅ EXITO: Leyendo credenciales desde secrets.json');
+    // 1. Intentamos leer de la memoria del servidor (Nube / GitHub Actions)
+    var envClientId = java.lang.System.getenv('AZURE_CLIENT_ID');
+    var envClientSecret = java.lang.System.getenv('AZURE_CLIENT_SECRET');
+    var envUsername = java.lang.System.getenv('AZURE_USERNAME');
+    var envPassword = java.lang.System.getenv('AZURE_PASSWORD');
 
-        // 3. Mapeamos los datos del JSON a nuestras variables globales
-        config.authClientId = secretosLocales.CLIENT_ID;
-        config.authClientSecret = secretosLocales.CLIENT_SECRET;
-        config.authUsername = secretosLocales.USERNAME;
-        config.authPassword = secretosLocales.PASSWORD;
+    // 2. Lógica Híbrida: Si existen en memoria, las usamos. Si no, vamos al archivo local.
+    if (envClientId && envClientSecret && envUsername && envPassword) {
+        karate.log('☁️ EXITO: Leyendo credenciales desde Variables de Entorno (Nube)');
 
-    } catch (e) {
-        karate.log('⚠️ ADVERTENCIA: No se encontró el archivo secrets.json');
+        config.authClientId = envClientId;
+        config.authClientSecret = envClientSecret;
+        config.authUsername = envUsername;
+        config.authPassword = envPassword;
+
+    } else {
+        karate.log('💻 INFO: No se detectaron variables de entorno. Buscando secrets.json local...');
+
+        try {
+            var secretosLocales = karate.read('file:config/secrets.json');
+
+            config.authClientId = secretosLocales.CLIENT_ID;
+            config.authClientSecret = secretosLocales.CLIENT_SECRET;
+            config.authUsername = secretosLocales.USERNAME;
+            config.authPassword = secretosLocales.PASSWORD;
+
+            karate.log('✅ EXITO: Leyendo credenciales locales desde secrets.json');
+        } catch (e) {
+            karate.log('⚠️ ERROR: Tampoco se encontró el archivo secrets.json local.');
+        }
     }
 
-    // 4. Retornamos el objeto. Karate inyectará estas variables en todos los .feature
     return config;
 }
